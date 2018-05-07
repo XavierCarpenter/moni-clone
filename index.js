6d3a598b472446a29dbf08c970c07d4e3e5c26d3
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-05-06 20:25:26 -0500

    updates to clone

---
+++ index.js
@@ -0,0 +1,20 @@
+window.onscroll = () => {
+  const nav = document.querySelector("#navbar");
+  if (this.scrollY <= 10) nav.className = "";
+  else nav.className = "scroll";
+};
+
+// window.onscroll = () => {
+//   const nav = document.querySelector("#navbar");
+//   if (this.scrollY <= 10) {
+//     $(".logo img").attr(
+//       "src",
+//       "https://monimymonidotcomprdsa.blob.core.windows.net/sitefinity/images/default-source/landing-page/moni-logo-w.png?sfvrsn=d76be1e6_2"
+//     );
+//   } else {
+//     $(".logo img").attr(
+//       "src",
+//       "https://monimymonidotcomprdsa.blob.core.windows.net/sitefinity/images/default-source/landing-page/moni-logo-c.png?sfvrsn=3064e1e6_2"
+//     );
+//   }
+// };
